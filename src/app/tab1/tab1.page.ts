96c900239671f21ee2cd9fd4e0ae1348d45eeb5c
alienpro <alienpro@naver.com>
2022-05-23 23:59:23 +0900

    tab1 ts, html 수정

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,8 +14,8 @@ import { PopoverController } from '@ionic/angular';
 export class Tab1Page {
 
   constructor(
-    private MGservice: MGserveService, 
-    private popover:PopoverController
+    public MGservice: MGserveService, 
+    private popover:PopoverController,
   ) {}
 
   randomAnimalXYparing = {}
@@ -41,27 +41,59 @@ export class Tab1Page {
   firstSelectedPic = "";
   secondSelectedpic = "";
   clickCount = 0;
+  matchedCardXY = [];
+  totalClickCount = 0;
+  audio = new Audio();
+  americanBritish = ["american", "british"]
   
   clickCard(row:string, col:string) {
+
+    let rand = Math.floor(Math.random()*this.americanBritish.length);
+    let pronouciation = this.americanBritish[rand];
+    
     if (this.clickCount == 0) {
       this.firstPicBlank = true;
       this.firstClickedXY = row.toString()+col.toString(); 
       this.firstSelectedPic = this.randomAnimalXYparing[this.firstClickedXY];
       this.clickCount++;
       this.secondPicBlank = false;
+      this.audio.src = "../../assets/soundFolder/" + this.firstSelectedPic.slice(0, -5)+"("+pronouciation+").mp3";
+      console.log(this.audio.src)
+      this.audio.load();
+      this.audio.play();
     } else  {
       this.secondClickedXY = row.toString()+col.toString();
       if (this.firstClickedXY != this.secondClickedXY) {
         this.secondPicBlank = true;
         this.secondSelectedpic = this.randomAnimalXYparing[this.secondClickedXY];
+
+        if (this.firstSelectedPic != this.secondSelectedpic) {
+          this.totalClickCount++;
+        }
         this.clickCount = 0;
+        this.audio.src = "../../assets/soundFolder/" + this.secondSelectedpic.slice(0, -5)+"("+pronouciation+").mp3";
+        console.log(this.audio.src)
+        this.audio.load();
+        this.audio.play();
       }
-      
-    }    
+    }
+    
+    if (this.firstSelectedPic == this.secondSelectedpic && this.clickCount == 0) {
+      this.totalClickCount++;
+      this.matchedCardXY.push(this.firstClickedXY, this.secondClickedXY);
+    }
+
+    // this.nativeAudio.preloadSimple('test', '../../assets/anaconda(amerian).mp3').then(()=>console.log('haha'));
+    // this.nativeAudio.play('test');
+
+    
+    
     
   }
 
   newGame() {
+    this.totalClickCount = 0;
+    this.matchedCardXY = [];
     this.MGservice.picTypeNum = this.MGservice.gameDimensionX * this.MGservice.gameDimensionY /2
     this.randomAnimalXYparing = this.MGservice.randomPositionPic();
     this.firstPicBlank = false;
@@ -76,21 +108,12 @@ export class Tab1Page {
     }
   }
 
-  // async createPopover(ev:any) {
-  //   const pop = await this.popover.create({
-  //     component:DimensionpopComponent,
-  //     event: ev
-  //   });
-    
-    
-  //   return pop.present()
 
-  // }
 
-  async createPopover(ev:any) {
+  async createPopover() {
     const pop = await this.popover.create({
       component:DimensionpopComponent,
-      event: ev
+      //event: ev
     });
     pop.present()
     
