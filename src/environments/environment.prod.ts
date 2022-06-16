0193c2f55da0c05efbd4df50bd4dc9214b83290a
ShinIl <alienpro@naver.com>
2022-06-17 06:00:03 +0900

    environment 설정

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     projectId: 'memorygame-61d03',
     appId: '1:529352933631:web:950007edf2eaa6c6ce0cca',
     storageBucket: 'memorygame-61d03.appspot.com',
+    locationId: 'asia-northeast1',
     apiKey: 'AIzaSyBQU0E1f22uhk9SqDF3unJ1qv5ELAwrnTs',
     authDomain: 'memorygame-61d03.firebaseapp.com',
     messagingSenderId: '529352933631',
