00c327f502f2bbba492455bd9249749d7729110b
ShinIl <alienpro@naver.com>
2022-06-19 07:38:26 +0900

    authentication 함수 마무리, auth-guard service 추가

---
+++ src/app/authentication/authentication.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,  UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(private readonly auth: Auth, private readonly router: Router) {}
+
+  canActivate():  Promise<boolean | UrlTree> {
+      return new Promise((resolve, reject) => {
+        onAuthStateChanged(this.auth, (user) => {
+          if(user) {
+            resolve(true);
+          } else {
+            reject('No user logged in');
+            this.router.navigateByUrl('/login');
+          }
+        })
+      })
+  }
+  
+}
--- src/app/authentication/authentication.page.ts
+++ src/app/authentication/authentication.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-authentication',
@@ -12,7 +13,9 @@ export class AuthenticationPage implements OnInit {
   pageTitle = "Sign In";
   actionButtonText = "Sign In";
 
-  constructor(private readonly router:Router) { }
+  constructor(
+    private readonly router:Router,
+    private readonly auth: AuthenticationService) { }
 
   ngOnInit() {
     this.url = this.router.url.substr(1);
@@ -45,14 +48,33 @@ export class AuthenticationPage implements OnInit {
 
   async login(email:string, password:string) {
     console.log(email, password)
+    try {
+      await this.auth.login(email, password);
+      this.router.navigateByUrl('/tabs');
+    } catch(error) {
+      console.log("can't login!!")
+    }
   }
 
   async signup(email:string, password:string) {
     console.log(email, password);
+    try {
+      await this.auth.signup(email, password);
+      this.router.navigateByUrl('/tabs');
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   async resetPassword(email:string) {
     console.log(email);
+    try {
+      await this.auth.resetPassword(email);
+      console.log('email sent')
+      this.router.navigateByUrl('/tabs');
+    } catch(error) {
+      console.log('error : ', error);
+    }
   }
 
 }
