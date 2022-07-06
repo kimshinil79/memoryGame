1397577581fb69a965d2390a32f6b05c537e8bb6
ShinIl <alienpro@naver.com>
2022-07-07 06:55:23 +0900

    authform, firestoen 수정

--- src/app/authentication/authentication.page.ts
+++ src/app/authentication/authentication.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { addDoc, collection } from 'firebase/firestore';
 import { AuthenticationService } from './authentication.service';
 
 @Component({
@@ -12,10 +14,12 @@ export class AuthenticationPage implements OnInit {
   url: string;
   pageTitle = "Sign In";
   actionButtonText = "Sign In";
+  loginfail = false;
 
   constructor(
     private readonly router:Router,
-    private readonly auth: AuthenticationService) { }
+    private readonly auth: AuthenticationService,
+    private readonly firestore: Firestore) { }
 
   ngOnInit() {
     this.url = this.router.url.substr(1);
@@ -50,9 +54,11 @@ export class AuthenticationPage implements OnInit {
     console.log(email, password)
     try {
       await this.auth.login(email, password);
+
       this.router.navigateByUrl('/tabs');
     } catch(error) {
       console.log("can't login!!")
+      this.loginfail = true;
     }
   }
 
@@ -60,6 +66,11 @@ export class AuthenticationPage implements OnInit {
     console.log(email, password);
     try {
       await this.auth.signup(email, password);
+      const userId:string = this.auth.getUser().uid;
+      const userCollection = collection(this.firestore, 'users/'+userId+'/profile/');
+      
+      addDoc(userCollection, {id :userId, name:'김신일'})
+
       this.router.navigateByUrl('/tabs');
     } catch(error) {
       console.log(error);
--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Auth,createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, User, UserCredential } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
@@ -7,7 +8,7 @@ import { Observable, of } from 'rxjs';
 })
 export class AuthenticationService {
 
-  constructor(private readonly auth:Auth) { }
+  constructor(private readonly auth:Auth, private readonly firestore: Firestore) { }
 
   getUser(): User {
     return this.auth.currentUser;
