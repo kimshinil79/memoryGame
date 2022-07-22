409c57bd60e61df5bcefcb79a2f9772de6895e3b
alienpro <alienpro@naver.com>
2022-07-22 17:58:14 +0900

    시험 탭 수정2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -16,21 +16,46 @@ export class Tab2Page {
   itemListForExam = [];
   nonAnswerItemsList = [];
   examNum = 10;
+  finalList = [];
   pictureIndex = ['0','1','2','3','4'];
-  biggerList = [[1,2,3,], [4,5,6]]
+  selectedIndex = "0"
+  test = "../../assets/Pic/ant2.jpg"
 
   ngOnInit() {
     this.newExam();
   }
 
+
+
   newExam() {
+
+    this.totalItemsList = [];
+    this.itemListForExam = [];
+    this.nonAnswerItemsList = [];
+    this.finalList = [];
+
+    this.selectedIndex = this.MGservice.suffleArray(this.pictureIndex)[0];
+
     for (let list of this.MGservice.categoriesList) {
       this.totalItemsList.push(...this.MGservice[list])
     }
-    this.itemListForExam = this.MGservice.suffleArray(this.totalItemsList).slice(0, this.examNum);
-    this.nonAnswerItemsList = this.totalItemsList.slice(10, this.totalItemsList.length-this.examNum)
-    let selectedIndex = this.MGservice.suffleArray(this.pictureIndex)[0]
+    this.itemListForExam = this.MGservice.suffleArray(this.totalItemsList).slice(0, this.examNum).map(
+      name =>"../../assets/Pic/" + name + this.selectedIndex + ".jpg");
+    this.nonAnswerItemsList = this.totalItemsList.slice(10, this.totalItemsList.length-this.examNum).map(
+      name =>"../../assets/Pic/" + name + this.selectedIndex + ".jpg");;
+    
 
+    let nonAnswerNum = 3;
+    let startIndexForSlice = 0
+    for (let answerItem of this.itemListForExam) {
+      let tempList = [];
+      tempList.push(answerItem);
+      tempList.push(...this.nonAnswerItemsList.slice(startIndexForSlice, startIndexForSlice+nonAnswerNum))
+      this.MGservice.suffleArray(tempList);
+      this.finalList.push(tempList)
+      startIndexForSlice = startIndexForSlice+3;
+
+    }
 
   }
 
