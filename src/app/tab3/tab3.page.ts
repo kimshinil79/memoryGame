0478ab8b10a2f088e3742ccd74c90db2bb932d0e
alienpro <alienpro@naver.com>
2022-10-10 21:32:07 +0900

    tab3 수정

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { ScoreComponent } from '../popovers/score/score.component';
 import { MGserveService } from '../services/mgserve.service';
+import { Observable, of, fromEvent } from 'rxjs';
 
 
 @Component({
@@ -12,167 +13,20 @@ import { MGserveService } from '../services/mgserve.service';
 export class Tab3Page {
 
   constructor(
-    public MGservice: MGserveService,
-    private popover: PopoverController,
+
   ) {}
 
-  totalItemsList = [];
-  itemListForExam = [];
-  itemListforExamWithJpg = [];
-  nonAnswerItemsList = [];
-  nonAnswerItemsListWithJpg = [];
-  examNum = 10;
-  finalList = [];
-  pictureIndex = ['0','1','2','3','4'];
-  selectedIndex = "0"
-  answerIndexList = [];
-  selectedNumList = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
-  americanBritish = ["american", "british"]
-  audio = new Audio();
-
-  questionPage:number = 0;
-
-  newGame = true;
 
   ngOnInit() {
-    this.newExam();
-  }
-  ngAfterViewInit() {
-    console.log("loaded")
-    //this.displayFirstQuestion();
-    this.changeQuestionPage(0);
-  }
 
-
-
-  async newExam() {
-
-    this.totalItemsList = [];
-    this.itemListForExam = [];
-    this.itemListforExamWithJpg = [];
-    this.nonAnswerItemsList = [];
-    this.nonAnswerItemsListWithJpg = [];
-    this.finalList = [];
-    this.answerIndexList = [];
-    this.selectedNumList = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
-    this.questionPage = 0;
-    this.newGame = true;
-    
-
-    this.selectedIndex = this.MGservice.suffleArray(this.pictureIndex)[0];
-
-    for (let list of this.MGservice.categoriesList) {
-      this.totalItemsList.push(...this.MGservice[list])
-    }
-    this.itemListForExam = this.MGservice.suffleArray(this.totalItemsList).slice(0, this.examNum)
-    this.itemListforExamWithJpg = this.itemListForExam.map(name =>"../../assets/Pic/" + name + this.selectedIndex + ".jpg");
-
-    this.nonAnswerItemsList = this.totalItemsList.slice(10, this.totalItemsList.length-this.examNum)
-    this.nonAnswerItemsListWithJpg = this.nonAnswerItemsList.map(name =>"../../assets/Pic/" + name + this.selectedIndex + ".jpg");;
-    
-
-    let nonAnswerNum = 3;
-    let startIndexForSlice = 0
-    for (let answerItem of this.itemListforExamWithJpg) {
-      let tempList = [];
-      tempList.push(answerItem);
-      tempList.push(...this.nonAnswerItemsListWithJpg.slice(startIndexForSlice, startIndexForSlice+nonAnswerNum))
-      this.MGservice.suffleArray(tempList);
-      let answerIndex = tempList.indexOf(answerItem)
-      this.answerIndexList.push(answerIndex);
-      this.finalList.push(tempList)
-      startIndexForSlice = startIndexForSlice+3;
-
-    }
-
-    this.displayFirstQuestion();
-
-    
-
+    const button = document.querySelector('ion-button')
+    fromEvent(document, 'click').subscribe(
+      (value)=>console.log(value['clientX'])
+    )
 
   }
 
-  displayFirstQuestion() {
 
-    for (let pageNum=0;pageNum<10;pageNum++) {
-      let tempPageId = "page"+pageNum.toString();
-      let tempElement = document.getElementById(tempPageId)
-      if (tempPageId == "page0" && tempElement != null) {
-        console.log('page0')
-        tempElement.style.display = ""
-      } 
-      if (tempPageId !="page0" && tempElement != null) {
-        console.log(tempPageId)
-        tempElement.style.display = "none"
-      }
-      
 
-      for (let itemNum=0;itemNum<4;itemNum++) {
-        let tempId = "test"+pageNum.toString()+itemNum.toString();
-        let tempItem =  document.getElementById(tempId)
-        if (tempItem !=null) {
-          tempItem.style.border = "none"
-        }
-        
-      }
-
-    }
-  }
-
-  makeSound(index) {
-    let rand = Math.floor(Math.random()*this.americanBritish.length);
-    let pronouciation = this.americanBritish[rand];
-    this.audio.src = "../../assets/soundFolder/" + this.itemListForExam[index]+"("+pronouciation+").mp3";
-    this.audio.load();
-    this.audio.play();
-  }
-
-  makeSelectedItem(questionNum, selectedNum) {
-
-    for (let index of ["0","1","2","3"]) {
-      let id = "test"+questionNum.toString()+index;
-      document.getElementById(id).style.border = "none"  
-    }
-    this.selectedNumList[questionNum] = selectedNum;
-    document.getElementById("test"+questionNum.toString()+selectedNum.toString()).style.border = "solid"
-    document.getElementById("test"+questionNum.toString()+selectedNum.toString()).style.borderColor = "red"
-    document.getElementById("test"+questionNum.toString()+selectedNum.toString()).style.borderWidth = "3px"
-    console.log(this.selectedNumList)
-  }
-
-  changeQuestionPage(pageNum) {
-
-    this.questionPage = pageNum;
-    let id = "question"+pageNum.toString();
-    for (let num=0;num<10;num++) {
-      let tempId = "question"+num.toString();
-      if (id == tempId) {
-        document.getElementById(id).style.display = "";
-      } else {
-        document.getElementById(tempId).style.display="none"
-      }
-    }
-  }
-
-  scoring() {
-    let rightAnswerNum = 0;
-    for (let i = 0;i<10;i++) {
-      if (this.answerIndexList[i] == this.selectedNumList[i]) {
-        rightAnswerNum = rightAnswerNum+1;
-      }
-    }
-    this.MGservice.score = rightAnswerNum*10;
-    this.scorePopover();
-
-  }
-
-  async scorePopover() {
-    const pop = await this.popover.create({
-      component: ScoreComponent
-    });
-    return pop.present()
-  }
-
-  
 
 }
